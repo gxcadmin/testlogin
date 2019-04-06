ff7567977e654aaaa6ec590dbaf2dec00a93fe3d
gaoxiaochu <gxc5623@sina.cn>
2019-04-06 22:53:31 +0800

    test

---
+++ src/components/Index/Index.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class='index'>
+        这是首页
+    </div>
+</template>
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+export default class Index extends Vue {}
+</script>
